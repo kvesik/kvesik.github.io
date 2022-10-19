ced6983ee316a5fbd1040d7fe198976f5057295f
kvesik <33950744+kvesik@users.noreply.github.com>
2022-10-19 01:17:34 -0700

    get talkmap working correctly (truncate online colocations)

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,47 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Seattle, Washington, USA",
+    47.6038321,
+    -122.330062
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Vancouver, British Columbia, Canada",
+    49.2608724,
+    -123.113952
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Chicago, Illinois, USA",
+    41.8755616,
+    -87.6244212
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Toronto, Ontario, Canada",
+    43.6534817,
+    -79.3839347
+  ],
+  [
+    "Seattle, Washington, USA / online",
+    47.656513000000004,
+    -122.37296171232529
+  ],
+  [
+    "Chicago, Illinois, USA / online",
+    41.8854961,
+    -87.6248895
+  ],
+  [
+    "Toronto, Ontario, Canada / online",
+    43.77945175,
+    -79.48981970940896
+  ],
+  [
+    "Marseille, France",
+    43.2961743,
+    5.3699525
+  ],
+  [
+    "Los Angeles, California, USA",
+    34.0536909,
+    -118.242766
   ]
 ];
