6cf196819f7ae71c62b306c34a21fd4ea910906d
kvesik <33950744+kvesik@users.noreply.github.com>
2022-10-19 01:27:08 -0700

    ensure talkmap doesn't try to plot online locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,38 +1,23 @@
 var addressPoints = [
   [
-    "Seattle, Washington, USA",
+    "Seattle, Washington, USA ",
     47.6038321,
     -122.330062
   ],
   [
-    "Vancouver, British Columbia, Canada",
+    "Vancouver, British Columbia, Canada ",
     49.2608724,
     -123.113952
   ],
   [
-    "Chicago, Illinois, USA",
+    "Chicago, Illinois, USA ",
     41.8755616,
     -87.6244212
   ],
   [
-    "Toronto, Ontario, Canada",
+    "Toronto, Ontario, Canada ",
     43.6534817,
     -79.3839347
-  ],
-  [
-    "Seattle, Washington, USA / online",
-    47.656513000000004,
-    -122.37296171232529
-  ],
-  [
-    "Chicago, Illinois, USA / online",
-    41.8854961,
-    -87.6248895
-  ],
-  [
-    "Toronto, Ontario, Canada / online",
-    43.77945175,
-    -79.48981970940896
   ],
   [
     "Marseille, France",
